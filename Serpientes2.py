d5a0e594989b23c616460c4c773aab564fdd6ef4
Jaime Daniel Torres <61173850+JaimeDanielTorres@users.noreply.github.com>
2020-03-10 22:21:56 -0600

    Update Serpientes2.py

--- Serpientes2.py
+++ Serpientes2.py
@@ -2,14 +2,15 @@ from tkinter import *
 from numpy import *
 from random import randint
 
-global c,aux,dado
+global c,aux,dado,turn
 dado=0
 c=0
 aux=0
+turn=1
 
 fila=array([3,0,0,0,0,0,0,0])    #0-7 / filas = 8
 columna=array([3,0,0,0,0,0,0,0,0,0,0,0,0])    #0-12 / columnas = 13
-datamatriz=array(150)
+datamatriz=[None]*100
 
 window = Tk()
 
@@ -17,27 +18,34 @@ window.title("Serpientes y Escaleras")
 
 window.geometry('700x500')
 
-#lbl = Label(window, text=c)
-
-#lbl.place(x=0, y=200, width=40, height=40)
-
 
 
        
 def boton():
-	global dado
+	global dado, turn
 	dado=randint(1,6)
 	print(dado)
 
 	buttondado = Button(window, text=dado, command=boton)
-	buttondado.place(x=560, y=274, width=60, height=60)
+	buttondado.place(x=560, y=275, width=60, height=60)
+
+	if(turn==1):
+		turn = 2
+		lbl = Label(window, text='Turno: '+str(turn),bg="blue")
+	else:
+		turn = 1
+		lbl = Label(window, text='Turno: '+str(turn),bg="red")
+
+	
+	lbl.place(x=560, y=200, width=100, height=40)
 
 
 
 for i in range(8):
     for j in range(13):
         op=i%2
-        c=c+1
+        c=c+1        
+
         button = 'btn'+str(c)
         button = Button(window, text=c)
 
@@ -46,20 +54,27 @@ for i in range(8):
         else:
         	h=42*j
 
-        v=42*(8-i)
+        v=42*(7-i)
 
-        if(c==92):
-        	h=h+84
-        	j=j+2
+        if(c>=92):
+        	if(c<=100):
+        		button.place(x=h+84, y=v, width=40, height=40)
+        		button = Button(window, bg="green")
+        	else:
+        		break
 
-        button.place(x=h, y=v, width=40, height=40)
-        button = Button(window, bg="green")
+        else:
+        	button.place(x=h, y=v, width=40, height=40)
+        	button = Button(window, bg="green")
                 
         print(c," - ",h, " - ",v)
 
 
 buttondado = Button(window, text="v", command=boton)
-buttondado.place(x=560, y=274, width=60, height=60)
+buttondado.place(x=560, y=275, width=60, height=60)
+
+lbl = Label(window, text='Turno: '+str(turn),bg="red")
+lbl.place(x=560, y=200, width=100, height=40)
 
 
 
@@ -74,4 +89,3 @@ buttondado.place(x=560, y=274, width=60, height=60)
 
 
 window.mainloop()
-
